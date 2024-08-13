17109333479c4b97b2ad4108af611a0b92cf4a50
srajansohani <srajansohani999@gmail.com>
2024-08-13 20:14:28 +0530

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import MainApp from "./routes";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, incrementByAmount } from "./redux/counter/counterSlice";
 import { Navbar } from "./components/Navbar";
-
-<<<<<<< HEAD
-function App(){
-
-  //Example for redux (to remove)
-  const count = useSelector((store)=> store.counter.value);
-  const dispatch = useDispatch();
-  console.log(count);
-  return <>
-    <Navbar />
-    <MainApp />
-
-
-  </>
-=======
 function App() {
     //Example for redux (to remove)
     const count = useSelector((store) => store.counter.value);
@@ -33,7 +18,6 @@ function App() {
     <h1>{count}</h1> */}
         </>
     );
->>>>>>> 91ec2c5935d805dbc4577407bf5da0b675bc86cb
 }
 
 export default App;
